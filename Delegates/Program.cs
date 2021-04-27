0661e3f1f99ed7271f0a5efb50783b5e8d0be8ae
Ryan Fiorini <ryanwfiorini@hotmail.com>
2021-04-27 01:27:27 -0400

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Delegates
+{
+    public delegate void MyDelegate();
+    public delegate void ShowTextDelegate(string text);
+
+    class Program
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("Inside Method1...");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Inside Method2...");
+        }
+        public static void ShowText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static void Main()
+        {
+            MyDelegate d = null;
+            d += Method1;
+            d += Method2;
+            d.Invoke();
+
+            ShowTextDelegate d2 = new ShowTextDelegate(ShowText);
+            d2("Hello World...");
+        }
+    }
+}
